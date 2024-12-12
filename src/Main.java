c9e40ea38a8c1740a6c6c57fe06c32a6217fe84c
char <char@Gandalf.home.local>
2024-12-12 21:39:58 +0100

    basic syntax -- until control flow

---
+++ src/Main.java
@@ -0,0 +1,168 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+
+
+        /**
+         * Variables
+         * String holds string data which is text
+         **/
+
+        String samy = "samyyyyy";
+        int number = 12;
+        boolean V = true;
+        Float nn = 4.545457F;
+        double allll = 4.524545645554;
+        char c = 'g';
+
+/**
+ * Operators
+ *   = assignment
+ *   + adding integers floats ...
+ *   + Concatenation
+ *   <  >  =< => == * ** / =! %
+ **/
+
+    //assignment
+        int x = 5;
+        int y = 3;
+        int z = 2;
+        z += y; // == z = z + y ;
+
+        z -= x;
+        z *= x;
+        z /= x;
+
+//        System.out.println(x + y);
+
+        // subtraction
+
+        int x1 = 5;
+        int y2 = 3;
+//        System.out.println(x - y);
+/**
+ *  = 	x = 5 	x = 5
+ *  += 	x += 3 	x = x + 3
+ *  -= 	x -= 3 	x = x - 3
+ *  *= 	x *= 3 	x = x * 3
+ *  /= 	x /= 3 	x = x / 3
+ *  %= 	x %= 3 	x = x % 3
+ *  &= 	x &= 3 	x = x & 3
+ *  |= 	x |= 3 	x = x | 3
+ *  ^= 	x ^= 3 	x = x ^ 3
+ *  >>= 	x >>= 3 	x = x >> 3
+ *  <<= 	x <<= 3 	x = x << 3
+ *
+ *  && == And
+ *  || == or
+ */
+
+//        int ty = 5;
+//        int f = 2;
+//        System.out.println( ty > f || x == z);
+
+
+        /**
+         * Collection Methods of given Variable
+         * String
+         **/
+
+//        String q = "    AIN CHOK    ";
+//        System.out.printf(q);
+//
+//        System.out.println(q.indexOf("C"));
+        // System.out.printf(q.replace("AIN","FOUARATE"));
+        //System.out.printf(q.concat("djkqhfkjsdhf"));
+      //  System.out.println(q.length());
+        //q.repeat(3);
+
+        /**
+         * escaping charachters
+         *
+         **/
+
+//       String dar = "salam ana mn \"beni mellal\" mryh f dar\t";
+//        System.out.print(dar);
+//        System.out.println(7);
+
+        /**
+         * Math
+         *
+         **/
+//         z = (int) Math.sqrt(9);
+//        System.out.println( (int)Math.pow(4,2));
+
+        // flow control or if else
+//        String user = "samy";
+//        if(user == "laila"){
+//
+//            System.out.println("Welcome : " + user);
+//
+//        }else{
+//            System.out.printf("user not valid");
+//        }
+//
+//        int doorCode = 1337;
+//
+//        if (doorCode == 1337) {
+//            System.out.println("Correct code. The door is now open.");
+//        } else {
+//            System.out.println("Wrong code. The door remains closed.");
+//        }
+//
+//        System.out.println("Please choose a number \n 1: charger solde \n 2: consulter facture \n 3: aide ");
+//        Scanner sc = new Scanner(System.in);
+//
+//
+//        switch (sc.nextInt()){
+//            case 1:
+//                System.out.println("enter 14 chiffre pour charger");
+//
+//                break;
+//            case 2:
+//                System.out.println("Votre facture a payer est 99DH");
+//                break;
+//            case 3:
+//                System.out.println("aide");
+//                break;
+//
+//        }
+
+//        int day = 4;
+//        switch (day) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//        }
+
+
+while
+    do while
+
+    for
+                arrays
+
+
+    }
+}
